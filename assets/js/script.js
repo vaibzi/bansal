a913f89b930c537ec1c9867699e199c3f286151a
Vaibhav <vaibz.work@gmail.com>
2024-12-09 23:29:25 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,74 @@
+'use strict';
+
+
+const playButton = document.querySelector(".play-btn");
+const videoPlayer = document.getElementById("videoPlayer");
+const videoBannerImage = document.querySelector(".video-banner .img-cover"); // Select the cover image
+
+playButton.addEventListener("click", () => {
+  // Hide the cover image and play button, show the video
+  videoPlayer.classList.add("video-playing");
+  playButton.style.display = "none"; // Hide play button
+  videoBannerImage.style.display = "none"; // Hide the cover image
+  videoPlayer.play();
+});
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+addEventOnElem(navLinks, "click", closeNavbar);
+
+
+
+/**
+ * header active when scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const activeElem = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", activeElem);
